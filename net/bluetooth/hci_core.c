6b4df96c983467cb1f34a2ca952c4217c3aa9f00
Johan Hedberg <johan.hedberg@intel.com>
2013-09-13 08:58:17 +0300

    Bluetooth: Introduce a new HCI_RFKILLED flag
    commit 5e130367d43ff22836bbae380d197d600fe8ddbb upstream.
    
    This makes it more convenient to check for rfkill (no need to check for
    dev->rfkill before calling rfkill_blocked()) and also avoids potential
    races if the RFKILL state needs to be checked from within the rfkill
    callback.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1126,8 +1126,7 @@ int hci_dev_open(__u16 dev)
 	/* Check for rfkill but allow the HCI setup stage to proceed
 	 * (which in itself doesn't cause any RF activity).
 	 */
-	if (test_bit(HCI_RFKILLED, &hdev->dev_flags) &&
-	    !test_bit(HCI_SETUP, &hdev->dev_flags)) {
+	if (test_bit(HCI_RFKILLED, &hdev->dev_flags)) {
 		ret = -ERFKILL;
 		goto done;
 	}
@@ -1551,8 +1550,7 @@ static int hci_rfkill_set_block(void *data, bool blocked)
 
 	if (blocked) {
 		set_bit(HCI_RFKILLED, &hdev->dev_flags);
-		if (!test_bit(HCI_SETUP, &hdev->dev_flags))
-			hci_dev_do_close(hdev);
+		hci_dev_do_close(hdev);
 	} else {
 		clear_bit(HCI_RFKILLED, &hdev->dev_flags);
 }
