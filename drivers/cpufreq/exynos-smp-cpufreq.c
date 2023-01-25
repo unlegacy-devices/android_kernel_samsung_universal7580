2f172cc4938e94e16ec0d6c266070a7cb8922b3b
NotNoelChannel <notnoel12345678@gmail.com>
2023-01-25 15:07:24 +0100

    Fix error #2

--- drivers/cpufreq/exynos-smp-cpufreq.c
+++ drivers/cpufreq/exynos-smp-cpufreq.c
@@ -825,8 +825,8 @@ static int exynos_cpufreq_tmu_notifier(struct notifier_block *notifier,
 		if (ret) {
 			mutex_unlock(&exynos_cpu_lock);
 			return NOTIFY_BAD;
+			}
 		}
-	}
 }
 
 static struct notifier_block exynos_tmu_nb = {
