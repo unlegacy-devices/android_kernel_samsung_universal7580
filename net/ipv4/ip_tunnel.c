bc36a19ee3edc87ed7995fa6ecf34f3aedaebecd
NotNoelChannel <notnoel12345678@gmail.com>
2023-12-04 18:08:19 +0100

    Revert "ip_tunnel: Do not use stale inner_iph pointer."
    This reverts commit c5a56ba7554700dd42f66121f4efd7b09e6b0bd7.

--- net/ipv4/ip_tunnel.c
+++ net/ipv4/ip_tunnel.c
@@ -646,7 +646,6 @@ void ip_tunnel_xmit(struct sk_buff *skb, struct net_device *dev,
 			tunnel->err_count = 0;
 	}
 
-	tos = ip_tunnel_ecn_encap(tos, inner_iph, skb);
 	ttl = tnl_params->ttl;
 	if (ttl == 0) {
 		if (skb->protocol == htons(ETH_P_IP))
@@ -676,11 +675,6 @@ void ip_tunnel_xmit(struct sk_buff *skb, struct net_device *dev,
 
 	skb_dst_drop(skb);
 	skb_dst_set(skb, &rt->dst);
-
-	err = iptunnel_xmit(dev_net(dev), rt, skb,
-			    fl4.saddr, fl4.daddr, protocol,
-			    tos, ttl, df);
-	iptunnel_xmit_stats(err, &dev->stats, dev->tstats);
 
 	/* Push down and install the IP header. */
 	skb_push(skb, sizeof(struct iphdr));
